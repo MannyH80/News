7a7bdd73846571895cfbf1da36d4586035830bfb
MannyH80 <mannyhusmc@gmail.com>
2020-08-19 17:52:36 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-5zfgg4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-5zfgg4)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+const but = document.getElementById('GetNews');
+
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
